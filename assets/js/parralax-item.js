dc88d2b3c6955d8ceb358934babcc663973ed994
abinyadihyah <54788774+abinyadihyah@users.noreply.github.com>
2022-01-23 14:06:24 +0700

    Add files via upload

---
+++ assets/js/parralax-item.js
@@ -0,0 +1,9 @@
+
+	$( document ).mousemove( function( e ) {
+
+		$( '.raw1' )	  .parallax( 30	, e );
+		$( '.raw2' )	  .parallax( 10	, e );
+		$( '.raw3' )	  .parallax( 50	, e );
+    $( '.raw4' )	  .parallax( 10	, e );
+
+	});
